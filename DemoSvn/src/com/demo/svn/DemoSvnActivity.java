176515be792bf082b080d8964c59adf07090a4b3
Konstant Info <mobileteamkonstant@gmail.com>
2011-12-01 17:47:38 +0530

    commit

---
+++ DemoSvn/src/com/demo/svn/DemoSvnActivity.java
@@ -0,0 +1,15 @@
+package com.demo.svn;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DemoSvnActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        
+    }
+}
